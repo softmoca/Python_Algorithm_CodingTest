8d39e5458472585fa9d97ce227b8b268db834384
softmoca <acyc0909@gmail.com>
2024-02-04 19:46:58 +0900

    k번째 약수

--- 파이썬알고리즘문제풀이입문/1.코드구현력기르기/나의코드/1.k번째약수.py
+++ 파이썬알고리즘문제풀이입문/1.코드구현력기르기/나의코드/1.k번째약수.py
@@ -1,1 +1,25 @@
+n,k=map(int,input().split())
+
+res=[]
+
+for i in range(1,n+1):
+    if n%i==0:
+        res.append(i)
+
+if len(res)>k-1:
+    print(res[k-1])
+else:
+    print(-1)
+
+
+
+
+
+
+
+
+
+
+
+
 
