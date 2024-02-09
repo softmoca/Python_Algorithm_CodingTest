9192ee56b4c36282963acf42094b1d0a20861b2d
softmoca <acyc0909@gmail.com>
2024-02-09 17:06:03 +0900

    후위 연산식 계산

--- 파이썬알고리즘문제풀이입문/4.자료구조(스택_큐_해쉬_힙)/나의코드/4.후위연산(스택).py
+++ 파이썬알고리즘문제풀이입문/4.자료구조(스택_큐_해쉬_힙)/나의코드/4.후위연산(스택).py
@@ -1,1 +1,22 @@
+arr=input()
+stack=[]
+
+for x in arr:
+    if x.isdecimal():
+        stack.append(int(x))
+    elif x =='+':
+        stack.append(stack.pop() + stack.pop())
+    elif x =='-':
+        temp=stack[-2] - stack[-1]
+        stack.pop()
+        stack.pop()
+        stack.append(temp)
+    elif x =='*':
+        stack.append(stack.pop() * stack.pop())
+    elif x =='/':
+        temp=stack[-2] / stack[-1]
+        stack.pop()
+        stack.pop()
+        stack.append(temp)
+print(stack[0])
 
