b4cff20c27aa7ed4b3a1a88b443cb31a10138659
softmoca <acyc0909@gmail.com>
2024-02-11 04:55:21 +0900

    수들의 조합

---
+++ 파이썬알고리즘문제풀이입문/5.DFS기초/나의코드/11.수들의조합.py
@@ -0,0 +1,24 @@
+def DFS(L,s):
+    global cnt
+    if L==k:
+        if sum(res)%m==0:
+            cnt+=1
+       
+    else:
+        for i in range(s,n):
+            res[L]=arr[i]
+            DFS(L+1,i+1)
+        
+
+
+n,k=map(int,input().split())
+arr=list(map(int,input().split()))
+m=int(input())
+
+res=[0]*k
+cnt=0
+DFS(0,0)
+
+
+
+print(cnt)
