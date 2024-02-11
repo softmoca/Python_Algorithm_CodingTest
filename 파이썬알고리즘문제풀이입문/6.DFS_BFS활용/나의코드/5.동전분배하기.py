bd5412ddf5e66a7161c65051ef6ab6b9a67bfb72
softmoca <acyc0909@gmail.com>
2024-02-11 19:06:45 +0900

    알파코드 못품

---
+++ 파이썬알고리즘문제풀이입문/6.DFS_BFS활용/나의코드/5.동전분배하기.py
@@ -0,0 +1,25 @@
+
+def DFS(L):
+    global Minn
+    if L==n:
+        if ch[0]!=ch[1] and ch[1]!=ch[2] and ch[0] != ch[2]:
+            tmp=max(ch)-min(ch)
+            if tmp<Minn:
+                print(ch)
+                Minn=tmp
+    else:
+        for i in range(3):
+            ch[i]+=arr[L]
+            DFS(L+1)
+            ch[i]-=arr[L]
+            
+
+
+n=int(input())
+arr=[]
+for _ in range(n):
+    arr.append(int(input()))
+ch=[0]*3
+Minn=100000
+DFS(0)
+print(Minn)
---
+++ 파이썬알고리즘문제풀이입문/6.DFS_BFS활용/나의코드/6.알파코드.py
@@ -0,0 +1,23 @@
+def DFS(L):
+    if L==len(arr):
+        print(res)
+    else:
+        for i in range(len(arr)):
+            for j in range(27):
+                if arr[i]==j
+
+
+        for i in range(27):
+            if i<10:
+                res[L]=i
+                DFS(L+1)
+
+
+arr=list(map(int,input()))
+
+print(arr)
+
+
+res=[]
+
+DFS(0)
