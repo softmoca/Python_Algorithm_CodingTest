56396cde9d4fce7aca5a01bff8ebcec5603fb60f
softmoca <acyc0909@gmail.com>
2024-02-24 21:50:01 +0900

    트리 순회

---
+++ 백준/단계별풀기/최단경로/1956.py
@@ -0,0 +1,30 @@
+V, E = map(int, input().split())
+#거리를 저장할 matrix
+graph = [[int(1e9)] * (V+1) for _ in range(V+1)]
+for _ in range(E):
+    x, y, c = map(int, input().split())
+    graph[x][y] = c
+
+#경유지 k, 출발지 i, 도착지 j 로 3중 for문을 돌면서
+for k in range(1, V+1):
+    for i in range(1, V+1):
+        for j in range(1, V+1):
+            # i->j 가 빠른지 i->k->j가 빠른지를 검사한다.
+             graph[i][j]=min(graph[i][j],  graph[i][k]+graph[k][j])
+graph
+answer = 1e9
+for i in range(1, V+1):
+   #사이클은 결국 출발지와 도착지가 같은 경우이므로 i->i를 확인
+    answer = min(answer, graph[i][i])
+
+#최소값이 없으면 -1, 있으면 최소값을 출력
+if answer == 1e9:
+    print(-1)
+else:
+    print(answer)
+
+
+
+
+
+
