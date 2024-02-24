76d8b9428e8235e323a98c9179ce2e94e7cb63ab
softmoca <acyc0909@gmail.com>
2024-02-24 21:52:41 +0900

    입문자 코테 2트 시작

---
+++ 입문자를 위힌 코딩테스트 핵심2/2회 배열과 연결리스트/기본 코드/4. 수열의 회전.py
@@ -0,0 +1,14 @@
+from collections import deque
+def solution(nums, k):
+    answer = []
+    
+    
+    return answer
+
+
+print(solution([3, 7, 1, 5, 9, 2, 8], 3))
+print(solution([2, 12, 2, 1, 3, 3, 9], 2))
+print(solution([1, 2, 5, 4, 6, 7, 9], 6))
+print(solution([1, 3, 6, 8, 14, 2, 1, 7], 5))
+
+
