358ae4ac85ea8e7885053f01fa6f44832b3da9cc
softmoca <acyc0909@gmail.com>
2024-02-25 19:53:08 +0900

    그리디 끝

--- 입문자를 위힌 코딩테스트 핵심2/6회 그리디(Greedy Algorithm)/기본 코드/4.카드점수[O(N^2)].py
+++ 입문자를 위힌 코딩테스트 핵심2/6회 그리디(Greedy Algorithm)/기본 코드/4.카드점수[O(N^2)].py
@@ -1,14 +1,11 @@
 def solution(nums, k):
     answer = 0
-    N=len(nums)
-    S=sum(nums)
-    block_size=N-k
+    
 
-    Minn=100
-    for i in range(N-block_size+1):
-        if Minn>sum(nums[i:i+block_size]):
-            Minn=sum(nums[i:i+block_size])
-    answer=S-Minn
+    for i in range(k):
+        if answer<sum(nums[:i]) + sum( nums[len(nums)-k+i:] ):
+            answer=sum(nums[:i]) + sum( nums[len(nums)-k+i:] )
+
     
 
     
@@ -21,3 +18,14 @@ print(solution([1, 2, 3, 5, 6, 7, 1, 3, 9], 5))
 print(solution([1, 30, 3, 5, 6, 7], 3))
 print(solution([1, 2, 15, 3, 6, 7, 8, 9], 5))
 print(solution([12, 5, 6, 12, 34, 35, 13, 3, 7, 8, 9], 7))
+
+
+
+
+
+
+
+
+
+
+
